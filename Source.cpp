ab6c283b7aaf53ef5cfe987b5f93291d34766c2d
franciscolinan <81978144+franciscolinan@users.noreply.github.com>
2021-09-01 23:14:12 -0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,6 @@
 #include "virtual_machine.h"
 
-int main()
+auto main() -> int
 {
 	std::uint32_t payload[] = { PUSH_REGISTER(EAX), POP_REGISTER(EBP), VM_EXIT };
 	virtual_machine::VMEntry(payload);
